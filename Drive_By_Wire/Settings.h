567989e1b9e19e0a8769b2ada3a671905b069c7d
lakeshaya75 <135804843+lakeshaya75@users.noreply.github.com>
2024-04-06 22:42:03 -0700

    Update Settings.h

--- Drive_By_Wire/Settings.h
+++ Drive_By_Wire/Settings.h
@@ -6,34 +6,59 @@
  * Do not check your local Settings.h into the repository.
  */
 
+
 #pragma once
+
 
 /*=======================================================================
 Minimum and maximum values to send to the motor
 Minimum and maximum speed allowed
 */
 
+// New variables for version 3 or earlier
+#define RC_STRAIGHT_MIN_MS 1000
+#define RC_STRAIGHT_MAX_MS 1850
+
+// New variables for version 4
+#define RC_STRAIGHT_MIN 511
+#define RC_STRAIGHT_MAX 511
+
 //min/max acceleration
 #define MIN_ACC_OUT 50
 #define MAX_ACC_OUT 227
 #define MAX_SPEED_KmPh 20
 
+
 // max speed
-const int32_t KmPh_mmPs = 1000000 / 3600;
-const int32_t MAX_SPEED_mmPs = MAX_SPEED_KmPh * KmPh_mmPs;  //at 20KMPH this is roughly 5,555
+const
+int32_t KmPh_mmPs =
+1000000 /
+3600;
+const
+int32_t MAX_SPEED_mmPs = MAX_SPEED_KmPh * KmPh_mmPs;  //at 20KMPH this is roughly 5,555
+
 
 // min speed -> slower is interpreted as stopped
 //set as 1% of the maximum speed -> 0.2 KmPh
-const int32_t MIN_SPEED_mmPs = 0.01 * MAX_SPEED_mmPs;
+const
+int32_t MIN_SPEED_mmPs =
+0.01 * MAX_SPEED_mmPs;
+
 
 /* Time that the brakes can be high */
-const uint32_t MaxHi_ms = 800;
+const
+uint32_t MaxHi_ms =
+800;
+
 
 /*=========================================================================
 
-Settings for the Steering 
+
+Settings for the Steering
+ 
 Minimum/Maximum and center turning signals
 */
+
 
 // Minimum turn in Degrees*1000 = Left 24 degrees
 #define MIN_TURN_Kdegrees -24000
@@ -43,6 +68,7 @@ Minimum/Maximum and center turning signals
 #define MIN_TURN_MS 1000
 // maximum steering servo pulse width in ms
 #define MAX_TURN_MS 1850
+
 
 /* There are wheel angle sensors on the left and right steering columns
    Each returns an analog voltage between 0 and 5 volts.
@@ -54,36 +80,46 @@ Minimum/Maximum and center turning signals
    These settings will vary significantly from vehicle to vehicle.
 */
 // These values have worked for the Carla simulator
+/*
 #define Left_Read_at_MIN_TURN 532
 #define Left_Read_at_MAX_TURN 206
 #define Right_Read_at_MIN_TURN 106
 #define Right_Read_at_MAX_TURN 532
-// These are some typical values
-//#define Left_Read_at_MIN_TURN 485
-//#define Left_Read_at_MAX_TURN 313
-//#define Right_Read_at_MIN_TURN 725
-//#define Right_Read_at_MAX_TURN 785
+*/
 
 
+// Orange Trike wheel angle sensor
+// wheel angle sensor had issues only using right sensor values
+#define Left_Read_at_MIN_TURN 532
+#define Left_Read_at_MAX_TURN 206
+#define Right_Read_at_MIN_TURN 673 // Min_turn =Hard right
+#define Right_Read_at_MAX_TURN 786 // Max_turn= Hard left
+#define Left_Straight_Read 731 // left side of drive
+#define Right_Straight_Read 731 // right side of drive
 /*======================================================================
 Vehicle Data
 Wheel Diameter, Turn Radius
 */
 
+
 #define WHEEL_DIAMETER_MM 495.3
 //derived settings
 const int32_t WHEEL_CIRCUM_MM = (WHEEL_DIAMETER_MM * PI);
+
 
 /*=======================================================================
 PID tuning for steering and throttle
 */
 
+
 #define PID_CALCULATE_TIME 50
+
 
 // Motor PID
 const double proportional_throttle = .0175;
 const double integral_throttle = .2;
 const double derivative_throttle = .00001;
+
 
 // Steering PID
 const double proportional_steering = .0175;
